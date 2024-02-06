fbeeb475cda2ec22ac5fe3dc7c1c0b9a8530abd5
Deepanshu Mishra <d4deepanshu723@gmail.com>
2024-02-07 00:46:01 +0530

    dd
    dd

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,8 @@ const Home = () => {
   return (
     <div className="text-center text-4xl font-bold">
       <h1>You Have Been Logged In!</h1>
+      <a href="/login" className="text-4xl" >Login</a>
+      <a href="/register" className="text-4xl">Register</a>
     </div>
   )
 }
